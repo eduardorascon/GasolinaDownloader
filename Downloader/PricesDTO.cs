658a3bd52fd23540a3fe00cc47553bed53bf58bb
Luis Eduardo Rascon Fierro <eduardo.rascon@gmail.com>
2017-08-28 12:37:53 -0500

    PricesDTO class created.

---
+++ Downloader/PricesDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Downloader.DTO
+{
+    public class PriceDTO
+    {
+        public string entidad { get; set; }
+        public string ciudad { get; set; }
+        public string magna { get; set; }
+        public string premium { get; set; }
+        public string diesel { get; set; }
+    }
+}
