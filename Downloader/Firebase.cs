c4af813f7fc5c1b5d5c161ba5d7fa9734269dfa6
Luis Eduardo Rascon Fierro <eduardo.rascon@gmail.com>
2017-08-27 20:35:23 -0500

    Classes moved into their own file.

---
+++ Downloader/Firebase.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Downloader
+{
+    public class FirebaseClient
+    {
+        public FirebaseClient()
+        {
+            string firebase_url = ConfigurationManager.AppSettings["firebase_url"];
+            HttpWebRequest request = WebRequest.CreateHttp(firebase_url);
+            request.Method = "POST";
+            request.ContentType = "application/json";
+
+            DateTime now = DateTime.Now;
+
+            //TODO
+            //To be replaced with string json = File.ReadAll(filename);
+            //Filename should be a .json file
+            string json = JsonConvert.SerializeObject(new
+            {
+                Name = now.ToString("yyyyMMdd"),
+                Value = now.ToString("HHmmss")
+            });
+
+            byte[] data = Encoding.UTF8.GetBytes(json);
+            request.ContentLength = data.Length;
+            request.GetRequestStream().Write(data, 0, data.Length);
+            WebResponse response = request.GetResponse();
+            json = new StreamReader(response.GetResponseStream()).ReadToEnd();
+        }
+    }
+}
---
+++ Downloader/Scrapper.cs
@@ -0,0 +1,64 @@
+﻿using HtmlAgilityPack;
+using ScrapySharp.Extensions;
+using ScrapySharp.Network;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace Downloader
+{
+    public class WebScrapper
+    {
+        public WebScrapper()
+        {
+            ScrapingBrowser Browser = new ScrapingBrowser();
+            Browser.AllowAutoRedirect = true;
+            Browser.AllowMetaRedirect = true;
+
+            string url = ConfigurationManager.AppSettings["url_to_scrap"];
+
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(url);
+
+            string file = string.Empty;
+            HtmlNode TitleNode = doc.DocumentNode.CssSelect(".article-body").First();
+            foreach (var anchor in TitleNode.ChildNodes.CssSelect("a"))
+            {
+                file = anchor.Attributes["href"].Value;
+                if (file.EndsWith(".xlsx"))
+                {
+                    //Response.Write(file + "<br />");
+                    DownloadExcelFile(file);
+                }
+            }
+        }
+        private void DownloadExcelFile(string address)
+        {
+            string excelFilesDirectory = HttpRuntime.AppDomainAppPath + ConfigurationManager.AppSettings["excel_storage"];
+            string fileName = excelFilesDirectory + GetFileName(address);
+
+            if (File.Exists(fileName))
+                return;
+
+            WebClient client = new WebClient();
+            try
+            {
+                client.DownloadFile(address, fileName);
+            }
+            catch (Exception)
+            {
+                address = address.Replace("https://", "http://");
+                client.DownloadFile(address, fileName);
+            }
+        }
+
+        private string GetFileName(string file)
+        {
+            int startIndex = file.LastIndexOf("/");
+            return file.Substring(startIndex);
+        }
+    }
+}
