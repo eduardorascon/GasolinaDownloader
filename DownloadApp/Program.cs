22059e42350bb4b98fd12f7c1a2eb11fbc339c75
Luis Eduardo Rascon Fierro <eduardo.rascon@gmail.com>
2017-09-20 23:00:32 -0500

    New project added.

---
+++ DownloadApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using DownloaderLibrary;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Web;
+
+namespace DownloadApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = ConfigurationManager.AppSettings["url_to_scrap"];
+            List<string> excelFiles = WebScrapper.LookupForExcelFiles(url);
+            string excelFilesDirectory = HttpRuntime.AppDomainAppPath + ConfigurationManager.AppSettings["excel_storage"];
+            excelFiles = WebScrapper.DownloadExcelFiles(excelFilesDirectory, excelFiles);
+
+        }
+    }
+}
