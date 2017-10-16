34d41a67b91609aaa3f1cc659eadf2f95787092d
Luis Eduardo Rascon Fierro <eduardo.rascon@gmail.com>
2017-10-15 21:59:42 -0500

    Organizing usings.

--- GasolinaDataUpdater/PricesDTO.cs
+++ GasolinaDataUpdater/PricesDTO.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace DownloaderLibrary.DTO
+﻿namespace DownloaderLibrary.DTO
 {
     public class PriceDTO
     {
