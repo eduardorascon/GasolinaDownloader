079048fd792a1d1c4fe9927a8f6a17cc77794449
Luis Eduardo Rascon Fierro <eduardo.rascon@gmail.com>
2017-10-21 22:24:18 -0500

    Comments added and a small refactoring.

--- JsonFilesGenerator/Main.cs
+++ JsonFilesGenerator/Main.cs
@@ -17,18 +17,23 @@ namespace JsonFilesGenerator
 
         private void btnGenerar_Click(object sender, EventArgs e)
         {
+            //1. First les get things setup.
             btnGenerar.Enabled = false;
             string excelFilesDirectory = ConfigurationManager.AppSettings["excel_storage"];
             DateTime startDate = new DateTime(2017, 1, 1);
             DateTime endDate = DateTime.Today;
 
-            List<string> excelFiles = new List<string>();
+            //2. We need to update firebase from the startDate to the current date.
             while (startDate <= endDate)
             {
+                //3. Lest list the excel file if its name matches with the startDate variable.
                 string excelDir = Path.Combine(excelFilesDirectory, startDate.ToString("MM"));
                 string[] startExcelFile = Directory.GetFiles(excelDir, startDate.ToString("yyyyMMdd") + "*");
-                excelFiles.Add(startExcelFile[0]);
+                string excelFile = startExcelFile[0];
+                //4. Updating firebase
+                UpdatePrecios(excelFile);
 
+                //5. startDate value is updated to the next available date.
                 string excelFilename = Path.GetFileNameWithoutExtension(startExcelFile[0]);
                 if (excelFilename.Contains("-"))
                     startDate = DateTime.ParseExact(excelFilename.Split('-')[1], "yyyyMMdd", CultureInfo.InvariantCulture);
@@ -36,8 +41,7 @@ namespace JsonFilesGenerator
                 startDate = startDate.AddDays(1);
             }
 
-            UpdateEstados(excelFiles);
-            UpdatePrecios(excelFiles);
+            UpdateEstados(excelFile);
         }
 
         private void UpdatePrecios(string excelFile)
